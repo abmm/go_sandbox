3769cc1974c2476dadee128f143eecd94e8b5e6c
abmm <Abian83@gmail.com>
2015-10-25 13:20:51 +0100

    Added Godep to install dependencies

--- 03_redis.go
+++ 03_redis.go
@@ -1,75 +1,91 @@
-package main 
+package main
 
 import (
-	"os"
 	"bufio"
-	"log"
 	"fmt"
-	"redis"
-	"strconv"
-	"time"
-	"sync/atomic"
+	"github.com/abmm/go_sandbox/Godeps/_workspace/src/redis"
+	"log"
+	"os"
 	"runtime"
+	"strconv"
+	"sync/atomic"
+	"time"
 )
 
-
 func addSingleKey(key string, new_value string) {
-	spec 			:= redis.DefaultSpec()
-	client, e := redis.NewSynchClientWithSpec (spec);
-	if e != nil { log.Println ("failed to create the client", e); return }
+	spec := redis.DefaultSpec()
+	client, e := redis.NewSynchClientWithSpec(spec)
+	if e != nil {
+		log.Println("failed to create the client", e)
+		return
+	}
 
-	value, e := client.Get(key);
-	if e!= nil { log.Println ("error on Get", e); return }
+	value, e := client.Get(key)
+	if e != nil {
+		log.Println("error on Get", e)
+		return
+	}
 	if value == nil {
 		value2set := []byte(new_value)
-		client.Set(key, value2set);
+		client.Set(key, value2set)
 		runtime.Gosched()
-	}else{
+	} else {
 		fmt.Println("Already found " + string(key))
 	}
 	//time.Sleep(time.Millisecond * 10)
 
 }
 
-
 func addKey(key string, new_value string, i *int32) {
-	spec 			:= redis.DefaultSpec()
-	client, e := redis.NewSynchClientWithSpec (spec);
-	if e != nil { log.Println ("failed to create the client", e); return }
-	for{
+	spec := redis.DefaultSpec()
+	client, e := redis.NewSynchClientWithSpec(spec)
+	if e != nil {
+		log.Println("failed to create the client", e)
+		return
+	}
+	for {
 		atomic.AddInt32(i, 1)
-		if *i > MAXKEYS{
+		if *i > MAXKEYS {
 			done <- true
-		}else{
-			if *i % 100000 == 0{
-				fmt.Printf("Inserted %d \n" ,*i)
+		} else {
+			if *i%100000 == 0 {
+				fmt.Printf("Inserted %d \n", *i)
 			}
 		}
 		key := key + strconv.Itoa(int(*i))
-		value, e := client.Get(key);
-		if e!= nil { log.Println ("error on Get", e); return }
+		value, e := client.Get(key)
+		if e != nil {
+			log.Println("error on Get", e)
+			return
+		}
 		if value == nil {
 			value2set := []byte(new_value)
-			client.Set(key, value2set);
+			client.Set(key, value2set)
 			runtime.Gosched()
-		}else{
+		} else {
 			//fmt.Println("Already found " + string(key))
 		}
 		//time.Sleep(time.Millisecond * 10)
 	}
 }
 
-func delKey(key string, i *int32){
-	spec 			:= redis.DefaultSpec()
-	client, e := redis.NewSynchClientWithSpec (spec);
-	if e != nil { log.Println ("failed to create the client", e); return }
-	for{
-		
-		if *i > MAXKEYS/2{	
+func delKey(key string, i *int32) {
+	spec := redis.DefaultSpec()
+	client, e := redis.NewSynchClientWithSpec(spec)
+	if e != nil {
+		log.Println("failed to create the client", e)
+		return
+	}
+	for {
+
+		if *i > MAXKEYS/2 {
 			s := strconv.Itoa(int(*i))
 			key := key + s
-			value, e := client.Get(key);
-			if e!= nil { log.Println ("error on Get", e); return }
+			value, e := client.Get(key)
+			if e != nil {
+				log.Println("error on Get", e)
+				return
+			}
 			if value != nil {
 				client.Del(key)
 				fmt.Println("Deleted " + key)
@@ -81,27 +97,26 @@ func delKey(key string, i *int32){
 	}
 }
 
-
-
 const MAXKEYS int32 = 1000000
+
 var totalkeys int32 = 0
 
-func start_redis () {
+func start_redis() {
 
-	fmt.Printf("\nIntroduce the key to save in Redis:");
-	reader := bufio.NewReader(os.Stdin);
-	key, _ := reader.ReadString(byte('\n'));
+	fmt.Printf("\nIntroduce the key to save in Redis:")
+	reader := bufio.NewReader(os.Stdin)
+	key, _ := reader.ReadString(byte('\n'))
 	if len(key) > 1 {
-		key = key[0:len(key)-1];
+		key = key[0 : len(key)-1]
 	}
 
 	t0 := time.Now()
-	go addKey(string(key), "valorx",&totalkeys)
-	go addKey(string(key), "valory",&totalkeys)
+	go addKey(string(key), "valorx", &totalkeys)
+	go addKey(string(key), "valory", &totalkeys)
 	//go delKey(string(key),&totalkeys)
 
 	//wait until done
-	<- done
+	<-done
 	t1 := time.Now()
 	fmt.Printf("Finish one million!! in %v \n", t1.Sub(t0))
 
--- 04_twitter_api.go
+++ 04_twitter_api.go
@@ -1,36 +1,35 @@
-package main 
+package main
 
 import (
- "github.com/ChimeraCoder/anaconda"
-	"os"
-  "fmt"
-	"time"
 	"bufio"
+	"fmt"
+	"github.com/abmm/go_sandbox/Godeps/_workspace/src/github.com/ChimeraCoder/anaconda"
 	"net/url"
+	"os"
+	"time"
 )
 
-
-func start_twitter_api () {
+func start_twitter_api() {
 	ACCESS_TOKEN_SECRET := os.Getenv("ACCESS_TOKEN_SECRET")
-	ACCESS_TOKEN 				:= os.Getenv("ACCESS_TOKEN")
-	CONSUMER_KEY 				:= os.Getenv("CONSUMER_KEY")
-	CONSUMER_SECRET 		:= os.Getenv("CONSUMER_SECRET")
+	ACCESS_TOKEN := os.Getenv("ACCESS_TOKEN")
+	CONSUMER_KEY := os.Getenv("CONSUMER_KEY")
+	CONSUMER_SECRET := os.Getenv("CONSUMER_SECRET")
 
 	anaconda.SetConsumerKey(CONSUMER_KEY)
 	anaconda.SetConsumerSecret(CONSUMER_SECRET)
 	api := anaconda.NewTwitterApi(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-	fmt.Printf("\nIntroduce the tag to find in Tweeter (max 10 characters):");
-	reader := bufio.NewReader(os.Stdin);
-	tag, _ := reader.ReadString(byte('\n'));
+	fmt.Printf("\nIntroduce the tag to find in Tweeter (max 10 characters):")
+	reader := bufio.NewReader(os.Stdin)
+	tag, _ := reader.ReadString(byte('\n'))
 	if len(tag) > 1 {
 		if len(tag) > 10 {
 			tag = tag[0:10]
-		}else{
-			tag = tag[0:len(tag)-1]
+		} else {
+			tag = tag[0 : len(tag)-1]
 		}
 	}
-	for{
+	for {
 
 		//api.PostTweet("probando api", nil)
 		v := url.Values{}
@@ -38,10 +37,10 @@ func start_twitter_api () {
 		v.Set("lang", "es")
 		searchResult, _ := api.GetSearch(string(tag), v)
 		i := 0
-		for _ , tweet := range searchResult.Statuses {
+		for _, tweet := range searchResult.Statuses {
 			i++
 			//Key =>  "tag/user/:user_id/tweet/:tweet_id/"
-			key 	:= tag + "/user/" + tweet.User.IdStr + "/" + "tweet/" + tweet.IdStr
+			key := tag + "/user/" + tweet.User.IdStr + "/" + "tweet/" + tweet.IdStr
 			value := tweet.Text
 
 			addSingleKey(key, value)
